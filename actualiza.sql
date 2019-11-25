9f8292b2edbddff34fb5fa96a1e31d5b0084f4c8
Angel Bernal Garcia <you@example.com>
2019-11-25 19:46:18 +0100

    actualiza crea ya tablas

--- actualiza.sql
+++ actualiza.sql
@@ -1,46 +1,44 @@
---/**********************************/
+﻿--/**********************************/
 --/		LANGUAGE	     /
 --/**********************************/
 create table languages(
 	language_id serial primary key,
-	language character varying(32) not null
+	language_str character varying(32) not null
 );
-insert into languages(language)
+insert into languages(language_str)
 select distinct language from imdb_movielanguages;
 
 alter table imdb_movielanguages
-	add column language_id integer,
-	add foreign key(language_id) references languages(language_id);
+	add column langid integer,
+	add foreign key(langid) references languages(language_id);
 
 update imdb_movielanguages as language_
-set language_id = (select language_id from languages where language = language_.language);
+set langid = (select language_id from languages where language_str = language_.language);
 
 alter table imdb_movielanguages
-	drop column extrainformation,
 	drop column language,
-	add constraint primary_language primary key (movieid, language_id);
+	add constraint primary_language primary key (movieid, langid);
 
 --/**********************************/
 --/		COUNTRY		     /
 --/**********************************/
 create table countries(
 	country_id serial primary key,
-	country character varying(32) not null
+	country_str character varying(32) not null
 );
-insert into countries(country)
+insert into countries(country_str)
 select distinct country from imdb_moviecountries;
 
 alter table imdb_moviecountries
-	add column country_id integer,
-	add foreign key(country_id) references countries(country_id)
+	add column countrid integer,
+	add foreign key(countrid) references countries(country_id);
 
 update imdb_moviecountries as country_
-set country_id = (select country_id from languages where language = language_.language);
+set countrid = (select country_id from countries where country_str = country_.country);
 
 alter table imdb_moviecountries
-	drop column extrainformation,
 	drop column country,
-	add constraint primary_country primary key (movieid, country_id);
+	add constraint primary_country primary key (movieid, countrid);
 
 
 
@@ -49,21 +47,20 @@ alter table imdb_moviecountries
 --/**********************************/
 create table genres(
 	genre_id serial primary key,
-	genre character varying(32) not null
+	genre_str character varying(32) not null
 );
-insert into genres(genre)
+insert into genres(genre_str)
 select distinct genre from imdb_moviegenres;
 
 alter table imdb_moviegenres
-	add column genres_id integer,
-	add foreign key(genre_id) references genres(genre_id)
+	add column genrid integer,
+	add foreign key(genrid) references genres(genre_id);
 
 update imdb_moviegenres as genre_
-set genre_id = (select genre_id from genres where genre = genre_.genre);
+set genrid = (select genre_id from genres where genre_str = genre_.genre);
 
 alter table imdb_moviegenres
-	drop column extrainformation,
 	drop column genre,
-	add constraint primary_genre primary key (movieid, genre_id);
+	add constraint primary_genre primary key (movieid, genrid);
 
 
--- setOrderAmount.sql
+++ setOrderAmount.sql
@@ -3,35 +3,15 @@ returns void as $$
 begin
 	update orders
 	set netamount = case when netamount is null then
-		rount((select sum(orderdetail.price*orderdetail.quantity)
+		round((select sum(orderdetail.price*orderdetail.quantity)
 		from orderdetail where orderid=orders.orderid), 2) else
 		netamount end,
 	totalamount = case when totalamount is null then
 		round((select sum(orderdetail.price*ordersdetail.quantity)
 		from orderdetail where orderid=orders.orderid)
-		*1(1+(tax/100)), 2) else
+		*(1+(tax/100)), 2) else
 		totalmount end;
 end;
-$$language plpgsqls;
+$$language plpgsql;
 
 select setOrderAmount();
-
-
-
-
--- select price
--- from products 
--- where products.prod_id in (select orderdetail.prod_id
--- 				from orderdetail, orders
---                                where orderdetail.orderid=orders.orderid);
--- 
--- insert into orders(netamount)
--- values (sum(neto)
--- 
--- 	where);
--- select netamount, tax, status, customerid, orderdate, totalamount
--- from orders
--- where orders.orderid=10
-
--- INSERT INTO (totalamount)
--- SELECT ((netamount*orders.tax)/100) from orders
