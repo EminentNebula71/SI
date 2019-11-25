5985c81fef42971635f6f9839e60eb07688b9c6b
EminentNebula71 <tyxy@hotmail.com>
2019-11-26 00:09:42 +0100

    weno

--- getTopMonths.sql
+++ getTopMonths.sql
@@ -1,3 +1,15 @@
-SELECT fecha
+CREATE OR REPLACE FUNCTION getTopMonths(numeric, numeric) RETURN TABLE(
+    mes INT,
+    dia INT,
+    importe_total INT,
+    cantidad_total INT
+) AS $$ DECLARE importe_min ALIAS FOR $1;
+DECLARE numero_min ALIAS FOR $2;
+BEGIN RETURN QUERY
+
+SELECT EXTRACT(year from orderdate) as year_aux, EXTRACT(day from orderdate) as day_aux, orderdetail.price*orderdetail.quantity AS sales_aux, sum(orderdetail.quantity) AS quantity_aux 
 FROM Orders JOIN orderdetail on Orders.orderid=orderdetail.orderid
 GROUP BY orders.orderdate 
+
+end;
+$$language plpgsql;
--- getTopVentas.sql
+++ getTopVentas.sql
@@ -1,4 +1,15 @@
-SELECT movietitle, count(*) as n
-FROM imdb_movies JOIN orderdetail on products.prod_id=orderdetail.prod_id
-WHERE imdb_movies.year=1999 
-GROUP BY imdb_movies.year ORDER BY n desc limit 1  
+CREATE OR REPLACE FUNCTION getTopVentas (numeric) RETURNS TABLE(
+  Year INT,
+  Title VARCHAR,
+  Sales INT
+) AS $$ DECLARE return_year ALIAS FOR $1;
+BEGIN RETURN QUERY
+
+SELECT TOP 1 year AS year_aux, movietitle AS title_aux, sum(orderdetail.quantity) AS sales_aux
+FROM orders INNER JOIN orderdetail ON orders.orderid=orderdetail.orderid,
+orderdetail INNER JOIN products ON orderdetail.prod_id=products.prod_id,
+products.prod_id=imdb_movies.movieid
+WHERE imdb_movies.year>=return_year
+
+end;
+$$language plpgsql;
