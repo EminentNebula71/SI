37dfe46dbcd1c4dad8ce0683d9af72910ffb869a
Angel Bernal Garcia <you@example.com>
2019-11-26 12:29:09 +0100

    getTopMonths iniciado

--- getTopMonths.sql
+++ getTopMonths.sql
@@ -1,13 +1,23 @@
-CREATE OR REPLACE FUNCTION getTopMonths(numeric, numeric) RETURN TABLE(
+﻿CREATE OR REPLACE FUNCTION getTopMonths(numeric, numeric) RETURN TABLE(
     mes INT,
     dia INT,
     importe_total INT,
     cantidad_total INT
 ) AS $$ DECLARE importe_min ALIAS FOR $1;
 DECLARE numero_min ALIAS FOR $2;
+
 BEGIN RETURN QUERY
 
-SELECT EXTRACT(year from orderdate) as year_aux, EXTRACT(day from orderdate) as day_aux, orderdetail.price*orderdetail.quantity AS sales_aux, sum(orderdetail.quantity) AS quantity_aux 
+SELECT CAST(query1.year_aux AS INTEGER), CAST(query1.month_aux AS INTEGER), CAST(precio_total AS INTEGER), CAST(cantidad_total AS INTEGER)
+FROM
+(SELECT EXTRACT( year from orders.orderdate) AS year_aux, EXTRACT( month FROM orders.orderdate) as month_aux, SUM(totalamount) as precio_total
+ FROM orders
+ GROUP BY EXTRACT(year FROM orders.orderdate), EXTRACT(month FROM orders.orderdate), SUM(totalamount), SUM(orderdetail.quantity)
+
+
+
+
+EXTRACT(year from orderdate) as year_aux, EXTRACT(day from orderdate) as day_aux, orderdetail.price*orderdetail.quantity AS sales_aux, sum(orderdetail.quantity) AS quantity_aux 
 FROM Orders JOIN orderdetail on Orders.orderid=orderdetail.orderid
 GROUP BY orders.orderdate 
 
