c6229edde9fbfcfe7082a25e7a6b4fade655e8c4
Angel Bernal Garcia <you@example.com>
2019-11-26 17:15:53 +0100

    slkha

--- getTopMonths.sql
+++ getTopMonths.sql
@@ -1,4 +1,4 @@
-﻿CREATE OR REPLACE FUNCTION getTopMonths(numeric, numeric) RETURN TABLE(
+﻿CREATE OR REPLACE FUNCTION getTopMonths(numeric, numeric) RETURNS TABLE(
     mes INT,
     dia INT,
     importe_total INT,
@@ -13,13 +13,16 @@ FROM
 (SELECT EXTRACT( year from orders.orderdate) AS year_aux, EXTRACT( month FROM orders.orderdate) as month_aux, SUM(totalamount) as precio_total
  FROM orders
  GROUP BY EXTRACT(year FROM orders.orderdate), EXTRACT(month FROM orders.orderdate), SUM(totalamount), SUM(orderdetail.quantity)
+)query1,
+(SELECT year_aux, MAX(sales_aux) as sales_aux
+ FROM orders
+ GROUP BY movietitle, EXTRACT(year from orders.orderdate)) AS ventasanio
+GROUP BY year_aux
 
-
-
-
-EXTRACT(year from orderdate) as year_aux, EXTRACT(day from orderdate) as day_aux, orderdetail.price*orderdetail.quantity AS sales_aux, sum(orderdetail.quantity) AS quantity_aux 
-FROM Orders JOIN orderdetail on Orders.orderid=orderdetail.orderid
-GROUP BY orders.orderdate 
-
+)
+-- EXTRACT(year from orderdate) as year_aux, EXTRACT(day from orderdate) as day_aux, orderdetail.price*orderdetail.quantity AS sales_aux, sum(orderdetail.quantity) AS quantity_aux 
+-- FROM Orders JOIN orderdetail on Orders.orderid=orderdetail.orderid
+-- GROUP BY orders.orderdate 
+)
 end;
 $$language plpgsql;
